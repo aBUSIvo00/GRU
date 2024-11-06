aef65dfd143fcd0f8783f6e093f89fb2852acf00
Alessandro Busi <167356928+aBUSIvo00@users.noreply.github.com>
2024-11-06 10:09:54 +0100

    Add files via upload

---
+++ gru_busi/gru_busi/RadioComando.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gru
+{
+    internal class RadioComando
+    {
+        public string NumeroSeriale
+        {
+            get { return NumeroSeriale; }
+            set { NumeroSeriale = value; }
+        }
+        private Gru gruAssociata;
+
+        public RadioComando(string numeroSeriale, Gru gru)
+        {
+            NumeroSeriale = numeroSeriale;
+            gruAssociata = gru;
+        }
+
+        public void AlzaGru(int metri)
+        {
+            gruAssociata.AlzaGru(metri);
+        }
+
+        public void AbbassaGru(int metri)
+        {
+            gruAssociata.AbbassaGru(metri);
+        }
+
+        public void RiportaInPosizioneSicurezza()
+        {
+            gruAssociata.PosizioneSicura();
+        }
+
+        public double GetAltezzaGru()
+        {
+            return gruAssociata.GetAltezza();
+        }
+    }
+}
---
+++ gru_busi/gru_busi/gru.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gru
+{
+    internal class Gru
+    {
+        private string nSeriale;
+        public string NSeriale
+        {
+            get { return nSeriale; }
+        }
+        private string produttore;
+        public string Produttore
+        {
+            get { return produttore; }
+        }
+        private int pesoMax;
+        public int PesoMax
+        {
+            get { return pesoMax; }
+        }
+        private int altMin;
+        public int Altmin
+        {
+            get { return altMin; }
+            set { altMin = value; }
+        }
+
+        private int altBra;
+        public int AltBra
+        {
+            get { return altBra; }
+            set { altBra = value; }
+        }
+
+        private int altMax;
+        public int AltMax
+        {
+            get { return altMax; }
+            set { altMax = value; }
+        }
+
+        public Gru(string nSeriale, string produttore, int pesoMax, int altMin, int altBra)
+        {
+            this.nSeriale = nSeriale;
+            this.produttore = produttore;
+            this.pesoMax = pesoMax;
+            this.altMin = altMin;
+            this.altBra = altBra;
+        }
+
+        public void AlzaGru(int metri)
+        {
+            altBra = altBra++;
+        }
+        public void AbbassaGru(int metri)
+        {
+            altBra = altBra--;
+        }
+        public int GetAltezza()
+        {
+            return altBra;
+        }
+        public void PosizioneSicura()
+        {
+            altBra = altMin;
+        }
+    }
+}
